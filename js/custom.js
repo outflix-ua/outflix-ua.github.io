f97cfcd50350ffa989df702fa693814a86ea093e
Kikisito <bekikisito@gmail.com>
2024-05-19 02:49:41 +0200

    Base

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Obtener todos los elementos con la clase 'scroll-link'
+    const links = document.querySelectorAll('.scroll-link');
+
+    links.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevenimos el comportamiento predeterminado del enlace
+
+            // Obtener el valor del atributo data-scroll-to
+            const targetId = this.getAttribute('data-scroll-to');
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                if(targetId === 'index') {
+                    window.scrollTo({top: 0, behavior: 'smooth'});
+                } else {
+                    targetElement.scrollIntoView({ behavior: 'smooth' });
+                    window.scrollBy(0, -30); // 30px arriba para que no quede la cabecera tapada por la navbar
+                }
+            }
+        });
+    });
+});
